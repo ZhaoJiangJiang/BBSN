c31bb41a12cd93c73badbfa989c26180e5d0e952
ZhaoJiangJiang <41458538+ZhaoJiangJiang@users.noreply.github.com>
2022-08-23 15:40:43 +0900

    Add files via upload

--- aapd_bbsn.py
+++ aapd_bbsn.py
@@ -325,7 +325,7 @@ def do_eval(model, valid_iter):
 
 def do_test(model, test_iter, result_path):
     model.eval()
-    tail_1000 = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]
+    tail_1000 = util_get_tail_label()
     entire_true = []
     entire_pred = []
 
